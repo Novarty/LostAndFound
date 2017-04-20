6b2b369f8ab7eb2a709e21dab00acebef646980f
Novarty <artem.ivanov9616@gmail.com>
2017-04-21 01:10:52 +0300

    добавил связь в таблиц в модели Item

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
   resources :items
   devise_for :users, :controllers => { registrations: 'registration/registrations' }
-
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170409125532) do
+ActiveRecord::Schema.define(version: 20170418124910) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "items", force: :cascade do |t|
+    t.string   "title"
+    t.string   "description"
+    t.integer  "tag_id"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
