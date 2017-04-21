eb7606c85a7099b9dc1f57b48d271122c4e98a5e
ihowardos <asergey96@gmail.com>
2017-04-21 23:46:57 +0300

    added user's details: name, surname, tel, photo, birthday

--- app/controllers/registration/registrations_controller.rb
+++ app/controllers/registration/registrations_controller.rb
@@ -61,11 +61,11 @@ class Registration::RegistrationsController < Devise::RegistrationsController
   private
 
   def sign_up_params
-    params.require(:user).permit(:email, :password, :password_confirmation)
+    params.require(:user).permit(:email, :password, :password_confirmation, :name, :surname, :telephone, :birthday)
   end
 
   def account_update_params
-    params.require(:user).permit(:email, :password, :password_confirmation, :current_password)
+    params.require(:user).permit(:email, :password, :password_confirmation, :current_password, :name, :surname, :telephone, :birthday, :location, :photo)
   end
 
 end
