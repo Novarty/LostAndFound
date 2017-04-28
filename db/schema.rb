64c2db58e69b718f9c3b9923d74628146e233557
Sergey Antonov <asergey96@gmail.com>
2017-04-28 09:19:45 +0300

    fixed creating items, added :user_id field to items (#11)

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170422125043) do
+ActiveRecord::Schema.define(version: 20170428061107) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 20170422125043) do
     t.string   "description"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.integer  "user_id"
   end
 
   create_table "questions", force: :cascade do |t|
@@ -66,8 +67,7 @@ ActiveRecord::Schema.define(version: 20170422125043) do
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
-  
+
   add_foreign_key "taggings", "items"
   add_foreign_key "taggings", "tags"
 end
-
