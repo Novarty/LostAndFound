dc526b47187e71fd48fe73c7f64a7617ad614037
ihowardos <asergey96@gmail.com>
2017-04-29 18:34:08 +0300

    added answer model

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :answers
 	root 'items#index'
   resources :questions
   resources :items
--- db/schema.rb
+++ db/schema.rb
@@ -15,6 +15,13 @@ ActiveRecord::Schema.define(version: 20170428075444) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "answers", force: :cascade do |t|
+    t.integer  "question_id"
+    t.text     "question_answer"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+  end
+
   create_table "items", force: :cascade do |t|
     t.string   "title"
     t.string   "description"
