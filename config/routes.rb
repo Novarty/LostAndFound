d7ec54e9684a816a9b13192c7cbbe956b79d8b4f
ihowardos <asergey96@gmail.com>
2017-04-29 18:56:47 +0300

    added panel to view

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,9 @@ Rails.application.routes.draw do
 	root 'items#index'
   resources :questions
   resources :items
+
   # for search in tags
   get 'tags/:tag', to: 'items#index', as: "tag"
-  
+
   devise_for :users, :controllers => { registrations: 'registration/registrations' }
 end
